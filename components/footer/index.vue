208b4f480caeb7fd022ae48cc3ce70d5c6b6a54c
codedrinker <gwangchunlei@gmail.com>
2020-12-20 11:06:27 +0800

    更新源码地址

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="footer">
-     Copyright © 2020 小兔窝图床 · All Rights Reserved Powered · 最新优惠: <a href="https://urlify.cn/nMzmeq" target="_blank">UCloud 云主机一年 59 元</a> · 源码: <a href="https://urlify.cn/nMzmeq" target="_blank">点击下载</a> 
+     Copyright © 2020 小兔窝图床 · All Rights Reserved Powered · 最新优惠: <a href="https://urlify.cn/nMzmeq" target="_blank">UCloud 云主机一年 59 元</a> · 源码: <a href="https://github.com/xiaotuwo/xiaotuwo-client" target="_blank">点击下载</a> 
    </div>
 </template>
 <style scoped>
